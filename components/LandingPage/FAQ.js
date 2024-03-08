09840a026310cb7bef7354270b1f88ce20ecee9d
UzairChaudhary <uzairchaudhary.30@gmail.com>
2024-03-08 23:48:18 +0500

    adjust padding of FAQ section

--- components/LandingPage/FAQ.js
+++ components/LandingPage/FAQ.js
@@ -19,7 +19,7 @@ export default function FAQ() {
           answer: 'ECO is a unique program initiated by the UK government, supported by energy companies, aimed at assisting residents in England, Scotland, and Wales with enhancing their homes. The objective? To increase energy efficiency in homes and reduce energy expenses.'
         },
         {
-          question: 'When does ECO start?',
+          question: 'When does ECO Start?',
           answer: 'The ECO4 initiative commenced in April 2022 and is scheduled to end by March 2026.'
         },
         {
@@ -32,14 +32,14 @@ export default function FAQ() {
     <div className='flex flex-col justify-center items-center mx-60 mt-40 font-proxima-nova '>
         <h1 className='text-green_color font-extrabold text-4xl'>Frequently Asked Questions</h1>
 
-        <div className="hover:cursor-pointer flex flex-col gap-3 p-5 w-full">
+        <div className="hover:cursor-pointer flex flex-col gap-3 p-5 mt-10 w-full">
             {faqData.map((faq, index) => (
                 <div key={index} className="faq-item border p-3 rounded-lg bg-green_color text-white shadow-md">
-                    <div className="flex text-2xl justify-between gap-20 px-10 items-center " onClick={() => toggleAccordion(index)}>
+                    <div className=" flex py-3 text-2xl justify-between gap-20 px-10 items-center " onClick={() => toggleAccordion(index)}>
                         <span>{faq.question}</span>
                         {activeIndex === index ? <IoIosArrowDropup /> : <IoIosArrowDropdown />}
                     </div>
-                    {activeIndex === index && <div className="text-base mt-3 px-10">{faq.answer}</div>}
+                    {activeIndex === index && <div className="text-base mt-3 px-10 mb-3">{faq.answer}</div>}
                 </div>
             ))}
         </div>
