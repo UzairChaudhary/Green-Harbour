84ca4cced947c70e89afb0739f598847a506a358
UzairChaudhary <uzairchaudhary.30@gmail.com>
2024-03-11 21:03:33 +0500

    responsive hero section

--- components/LandingPage/HeroSection.js
+++ components/LandingPage/HeroSection.js
@@ -1,24 +1,22 @@
-import React from 'react'
-import Link from 'next/link'
+import React from 'react';
+import Link from 'next/link';
+
 export default function HeroSection() {
   return (
     <div className="relative bg-cover bg-center bg-no-repeat h-screen flex items-center justify-center" style={{backgroundImage: "url('/landingpage/herosection.png')"}}>
         <div className="flex flex-col justify-center items-center">
-            <div className='text-center mb-32 mt-20'>
-                <h1 className="text-5xl text-mud_color font-proxima-nova font-extrabold mb-2">Enhance your home at no cost with Green</h1>
-                <h1 className="text-5xl text-mud_color font-proxima-nova font-extrabold">Harbour ECO4 Government Grant Scheme!</h1>
-
+            <div className='text-center mb-10 mt-10'> {/* Adjusted top and bottom margins */}
+                <h1 className="text-4xl md:text-5xl text-mud_color font-proxima-nova font-extrabold mb-2">Enhance your home at no cost with Green</h1> {/* Adjusted text size */}
+                <h1 className="text-4xl md:text-5xl text-mud_color font-proxima-nova font-extrabold">Harbour ECO4 Government Grant Scheme!</h1> {/* Adjusted text size */}
             </div>
-            <div className='text-center mt-40'>
-                <p className="text-lg text-white font-proxima-nova font-bold mt-10">Explore how you can convert your home into an energy-efficient </p>
+            <div className='text-center mt-20'> {/* Adjusted top margin */}
+                <p className="text-lg text-white font-proxima-nova font-bold mt-2">Explore how you can convert your home into an energy-efficient </p> {/* Adjusted text size */}
                 <p className="text-lg text-white font-proxima-nova font-bold">sanctuary and start saving on your energy expenses today!</p>
-
             </div>
             <Link
             href={'/who-qualifies'} 
-            className="bg-orange_color hover:scale-105 hover:transition-a text-white font-bold py-3 text-lg px-6 mt-16 mb-10  rounded-lg">Register Today</Link>
+            className="bg-orange_color hover:scale-105 hover:transition-a text-white font-bold py-3 text-lg px-6 mt-12 mb-10  rounded-lg">Register Today</Link> {/* Adjusted button styling */}
         </div>
     </div>
-
-  )
+  );
 }
