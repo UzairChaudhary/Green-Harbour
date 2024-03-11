c62ecdb7658a5726c42c47dba0a9c34ec8524cd7
UzairChaudhary <uzairchaudhary.30@gmail.com>
2024-03-11 21:00:16 +0500

    responsive navbar

--- components/LandingPage/GetInTouch.js
+++ components/LandingPage/GetInTouch.js
@@ -20,9 +20,11 @@ export default function GetInTouch() {
         }
       };
   return (
-    <div className='mb-40 mx-40'>
+    <>
+    <div className='mb-40 flex flex-col items-center mx-40'>
+        <div>
         <h1 className='mt-20 flex justify-center text-4xl font-extrabold text-green_color'>Get In Touch</h1>
-        <div className='flex justify-center text-mud_color gap-10 mt-10'>
+        <div className='flex text-mud_color gap-10 mt-10'>
             <div className='flex text-mud_color flex-col gap-3'>
                 <h1>First Name</h1>
                 <input type='text' className='border rounded-md p-2 border-mud_color w-96'/>
@@ -32,7 +34,7 @@ export default function GetInTouch() {
                 <input type='text' className='border border-mud_color rounded-md p-2 w-96'/>
             </div>
         </div>
-        <div className='flex justify-center text-mud_color gap-10 mt-10'>
+        <div className='flex  text-mud_color gap-10 mt-10'>
             <div className='flex text-mud_color flex-col gap-3'>
                 <h1>Email</h1>
                 <input type='email' className='border rounded-md p-2 border-mud_color w-96'/>
@@ -42,19 +44,25 @@ export default function GetInTouch() {
                 <input type='text' className='border border-mud_color rounded-md p-2 w-96'/>
             </div>
         </div>
-        <div className='flex  text-mud_color mt-10 mx-48 px-2 flex-col gap-3'>
+        <div className='flex flex-col text-mud_color mt-10 px-2 gap-3'>
             <h1 >Message</h1>
             <textarea className='border border-mud_color rounded-md p-2 h-24'></textarea>
         </div>
-        <div className='flex hover:cursor-pointer mx-48 px-2 mt-5 justify-end'>
+
+
+        <div className='flex hover:cursor-pointer px-2 mt-5 justify-end'>
             <div
             onClick={handlesubmit} 
             className='bg-orange_color px-6 text-white py-1 rounded'>
                 Submit
             </div>
         </div>
+        </div>
+       
 
        
+    
     </div>
+    </>
   )
 }
