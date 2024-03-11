d5facaaa07209c544f07abfc82538805e58b8cbe
UzairChaudhary <uzairchaudhary.30@gmail.com>
2024-03-12 00:22:16 +0500

    responsive button component

--- components/LandingPage/Navbar.js
+++ components/LandingPage/Navbar.js
@@ -33,14 +33,14 @@ function Navbar() {
         <nav suppressHydrationWarning={true} className="bg-green_color flex justify-between items-center pt-2 px-4">
             {/* Logo on the left */}
             <Link href="/" suppressHydrationWarning={true}>
-                <h1 className="text-4xl p-3 font-extrabold text-mud_color">Green Harbour</h1>
+                <h1 className="sm:text-4xl text-2xl sm:p-3 pb-2 font-extrabold text-mud_color">Green Harbour</h1>
             </Link>
 
             {/* Hamburger menu for mobile view */}
             {isMobile ? (
                 <div>
                     <button onClick={() => dispatch({ type: actioTypes.toggleSidebar })}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                         </svg>
                     </button>
--- components/common/button.js
+++ components/common/button.js
@@ -3,7 +3,7 @@ export default function Button (){
     return(
         <Link
         href={'/who-qualifies'} 
-        className="block w-full sm:w-auto bg-orange-500 text-lg text-white py-2 px-4 rounded-lg text-center hover:scale-105 hover:transition-a hover:bg-orange-600">
+        className="block  sm:w-auto bg-orange-500 text-lg text-white py-2 px-4 rounded-lg text-center hover:scale-105 hover:transition-a hover:bg-orange-600">
             Register Today
         </Link>
     )
