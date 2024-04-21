746768f943a1e05dab76c2d729f7da875ae26636
UzairChaudhary <uzairchaudhary.30@gmail.com>
2024-04-21 14:01:42 +0500

    modify navbar

--- components/LandingPage/Footer.js
+++ components/LandingPage/Footer.js
@@ -5,7 +5,7 @@ import { FaPhone } from "react-icons/fa6";
 import { MdEmail } from "react-icons/md";
 export default function Footer() {
   return (
-    <div className='bg-green_color font-proxima-nova footer hover:cursor-pointer px-4 md:px-10 lg:px-20 py-6 md:py-10 text-white flex flex-row justify-between'>
+    <div className='bg-green_color gap-2 font-proxima-nova footer hover:cursor-pointer px-4 md:px-10 lg:px-20 py-6 md:py-10 text-white flex flex-row justify-between'>
     <div className='flex flex-col'>
             <Link href="/" suppressHydrationWarning={true} className=''>
                 {/* <h1 className="sm:text-4xl text-2xl sm:p-3 pb-2 font-extrabold text-mud_color">Green Harbour</h1> */}
